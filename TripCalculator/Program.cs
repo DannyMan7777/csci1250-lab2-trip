becd3404d90db4a6de0070c3aa22859033fdde9b
DannyMan7777 <unthinkableplayz28@gmail.com>
2026-09-23 14:03:19 -0400

    Add part 2 pizza party

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -1,15 +1,15 @@
 ﻿//Part 1: Road Trip
 
-Console.Write("Enter the round trip mileage: ");
-int tripMileage = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the round trip mileage (no nonnumeric values): ");
+double tripMileage = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Enter the vehicle's gas mileage: ");
-int gasMileage = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the vehicle's gas mileage (no nonnumeric values): ");
+double gasMileage = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Enter the price of gas: ");
+Console.Write("Enter the price of gas (no nonnumeric values): ");
 double gasPrice = Convert.ToDouble(Console.ReadLine());
 
-double gallonsNeeded = tripMileage / (double)gasMileage;
+double gallonsNeeded = tripMileage / gasMileage;
 
 double fuelCost = gallonsNeeded * gasPrice;
 
@@ -18,7 +18,29 @@ System.Console.WriteLine("Cost of fuel: " + fuelCost.ToString("C"));
 
 //Part 2: Pizza Party
 
+const int PIZZA_SLICES = 8;
 
+Console.Write("Enter the number of people going (no nonnumeric values): ");
+int numberGoing = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the number of pizzas to purchase (no nonnumeric values): ");
+int numberPizzas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the price per pizza (no nonnumeric values): ");
+double pizzaPrice = Convert.ToDouble(Console.ReadLine());
+
+int numberSlices = numberPizzas * PIZZA_SLICES;
+
+double slicesPerPerson = numberSlices / numberGoing;
+
+int leftoverSlices = numberSlices % numberGoing;
+
+double pizzaCost = numberPizzas * pizzaPrice;
+
+System.Console.WriteLine("Total slices of pizza (no nonnumeric values): " + numberSlices.ToString("F0"));
+System.Console.WriteLine("Slices per person (no nonnumeric values): " + ((int)slicesPerPerson).ToString("F0"));
+System.Console.WriteLine("Leftover slices (no nonnumeric values): " + leftoverSlices.ToString("F0"));
+System.Console.WriteLine("Total cost of pizzas (no nonnumeric values): " + pizzaCost.ToString("C"));
 
 //Part 3: Paycheck
 
